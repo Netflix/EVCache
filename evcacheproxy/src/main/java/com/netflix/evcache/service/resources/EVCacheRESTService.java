339c59d2f74b1d6920720a2430a1a0764a97cfae
Sridhar Enugula <senugula@netflix.com>
2016-06-23 17:00:01 -0700

    Adding Transcoder and eureka properties

--- evcacheproxy/src/main/java/com/netflix/evcache/service/resources/EVCacheRESTService.java
+++ evcacheproxy/src/main/java/com/netflix/evcache/service/resources/EVCacheRESTService.java
@@ -12,7 +12,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
