6a63f2912d959452dab9c8114abd275f84c6843a
Jason Koch <jkoch@netflix.com>
2025-01-29 15:56:35 -0800

    perf: Optimize the MD5 hashring algorithm to avoid Charset lookup

--- evcache-core/src/main/java/com/netflix/evcache/pool/HashRingAlgorithm.java
+++ evcache-core/src/main/java/com/netflix/evcache/pool/HashRingAlgorithm.java
@@ -3,6 +3,10 @@ package com.netflix.evcache.pool;
 import net.spy.memcached.DefaultHashAlgorithm;
 import net.spy.memcached.HashAlgorithm;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Description of a hash ring algorithm. A hash ring algorithm has a hash
  * function, and a way to split the hash into parts in the case of a
@@ -40,9 +44,33 @@ public interface HashRingAlgorithm {
     }
 
     static class KetamaMd5HashRingAlgorithm implements HashRingAlgorithm {
+        private static MessageDigest md5Digest = null;
+
+        static {
+            try {
+                md5Digest = MessageDigest.getInstance("MD5");
+            } catch (NoSuchAlgorithmException e) {
+                throw new RuntimeException("MD5 not supported", e);
+            }
+        }
+
         @Override
         public long hash(CharSequence key) {
-            return DefaultHashAlgorithm.KETAMA_HASH.hash(key.toString());
+            // manually inlined from DefaultHashAlgorithm, avoids the Charset lookup
+            long rv = 0;
+            MessageDigest md5;
+            try {
+              md5 = (MessageDigest) md5Digest.clone();
+            } catch (CloneNotSupportedException e) {
+              throw new RuntimeException("clone of MD5 not supported", e);
+            }
+            md5.update(key.toString().getBytes(StandardCharsets.UTF_8));
+            byte[] bKey = md5.digest();
+            rv = ((long) (bKey[3] & 0xFF) << 24)
+                    | ((long) (bKey[2] & 0xFF) << 16)
+                    | ((long) (bKey[1] & 0xFF) << 8)
+                    | (bKey[0] & 0xFF);
+            return rv & 0xffffffffL; /* Truncate to 32-bits */
         }
 
         @Override
@@ -61,5 +89,4 @@ public interface HashRingAlgorithm {
             }
         }
     }
-
 }
